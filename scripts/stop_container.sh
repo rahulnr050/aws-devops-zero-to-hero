aabb2f9c0ee665fec77e136e4bdf80d0453c40c1
rahulnr050 <rahulnr050@gmail.com>
2026-04-08 12:31:05 +0530

    nnn

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-# Get running container IDs only (no header)
-containerid=$(docker ps -q)
+echo "Stopping containers..."
+
+DOCKER=/usr/bin/docker
+
+containerid=$($DOCKER ps -q)
 
 if [ -n "$containerid" ]; then
-  docker rm -f $containerid
+  $DOCKER rm -f $containerid
 else
   echo "No running containers to stop"
 fi
